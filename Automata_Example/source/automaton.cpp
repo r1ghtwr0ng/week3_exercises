947834a9f55e62d00a3381553ff87e1290613bfa
Michael McLeod <michaelmcleod@ucl.ac.uk>
2024-01-25 16:17:01 +0000

    Add comments for std::map functionality

--- Automata_Example/source/automaton.cpp
+++ Automata_Example/source/automaton.cpp
@@ -6,7 +6,14 @@ bool Automaton::Read(string word)
 {
     for (auto &c : word)
     {
-        int j = alphabet.find(c)->second;
+        // a map's find method returns an iterator to the key-value pair for the given key
+        // iterators have syntax similar to pointers: 
+        // (*it) gives the key-value pair
+        // -> can be used to access methods of the key value pair
+        auto it = alphabet.find(c);
+
+        //it->first gives the key, it->second gives the value
+        int j = it->second;
         state = transition_matrix[state][j];
     }
 
